8d2fba51f387acf7807458c76b97f643a815f944
pichate-k <59151392+pichate-k@users.noreply.github.com>
2025-03-10 22:45:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 
 st.title("🎈 ระบบวิเคราะห์เอกสารสำหรับการรับงานพิมพ์")
+col1, col2 = st.columns(2)
+with col1:
+    st.header("Column 1")
+    st.write("Some text or elements")
+with col2:
+    st.header("Column 2")
+    st.write("Some other elements")
+ 
+# Use an expander to hide details
+with st.expander("See details"):
+    st.write("Here are some detailed elements")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
