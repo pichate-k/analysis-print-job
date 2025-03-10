340ea9fc5b2a12d26fc3d66bdd488c4fcee7952c
pichate-k <59151392+pichate-k@users.noreply.github.com>
2025-03-10 22:46:46 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 st.title("🎈 ระบบวิเคราะห์เอกสารสำหรับการรับงานพิมพ์")
+
+# File uploader
+uploaded_file = st.file_uploader("Choose a file")
+
 col1, col2 = st.columns(2)
 with col1:
     st.header("Column 1")
@@ -12,6 +16,8 @@ with col2:
 # Use an expander to hide details
 with st.expander("See details"):
     st.write("Here are some detailed elements")
+'''
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+'''
