7695f26745db2fdff152e58aee4ebf1ed880efb4
pichate-k <59151392+pichate-k@users.noreply.github.com>
2025-03-10 22:28:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 ระบบวิเคราะห์เอกสารสำหรับการรับงานพิมพ์")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
